71d491173a1755362ba68ade3d98a6fdfe5fe8e8
arthurmachadoo <arthurmachadoferreira@gmail.com>
2025-11-26 20:39:27 -0300

    Injeção de dependências com CocoaPods

---
+++ Pods/Target Support Files/Pods-AMFlix_Project-AMFlix_ProjectUITests/Pods-AMFlix_Project-AMFlix_ProjectUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AMFlix_Project_AMFlix_ProjectUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AMFlix_Project_AMFlix_ProjectUITestsVersionString[];
+

4d5d73f415656636658f5da064790a81edc6967d
arthurmachadoo <arthurmachadoferreira@gmail.com>
2025-11-26 20:32:36 -0300

    Initial Commit

---
+++ AMFlix_Project/AMFlix_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMFlix_ProjectApp.swift
+//  AMFlix_Project
+//
+//  Created by Arthur Machado Ferreira on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AMFlix_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMFlix_ProjectTests/AMFlix_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  AMFlix_ProjectTests.swift
+//  AMFlix_ProjectTests
+//
+//  Created by Arthur Machado Ferreira on 26/11/25.
+//
+
+import Testing
+@testable import AMFlix_Project
+
+struct AMFlix_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
